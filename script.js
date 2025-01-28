d3d91d9c1cc3576937bad66a336f621e9260127e
Wellington <wellington_ctt@hotmail.com>
2025-01-27 23:57:36 -0300

    commit final

--- script.js
+++ script.js
@@ -38,20 +38,25 @@ async function dadosDiarios() {
     try {
         const dados = await fetch('./dados.json');
         const dadosJson = await dados.json();
-        const diasComFaturamento = dadosJson.filter(dia => dia.valor > 0);
 
+        const diasComFaturamento = dadosJson.filter(dia => dia.valor > 0);
+        
         const menorValor = diasComFaturamento.reduce((valMinAcc, dia) => {
             return dia.valor < valMinAcc ? dia.valor : valMinAcc;
-        }, diasComFaturamento[0].valor); 
+        }, diasComFaturamento[0].valor);
 
         const maiorValor = diasComFaturamento.reduce((valMaxAcc, dia) => {
             return dia.valor > valMaxAcc ? dia.valor : valMaxAcc;
-        }, diasComFaturamento[0].valor); 
+        }, diasComFaturamento[0].valor);
 
-        const mediaDiaMes = 
+        const somaFaturamento = diasComFaturamento.reduce((soma, dia) => soma + dia.valor, 0);
+
+        const mediaMensal = somaFaturamento / diasComFaturamento.length;
+        const diasAcimaDaMedia = diasComFaturamento.filter(dia => dia.valor > mediaMensal).length;
 
         console.log(`O menor valor de faturamento diário foi: ${menorValor}`);
         console.log(`O maior valor de faturamento diário foi: ${maiorValor}`);
+        console.log(`Número de dias com faturamento acima da média: ${diasAcimaDaMedia}`);
     } catch (error) {
         console.error("Erro ao buscar ou processar os dados:", error);
     }
@@ -59,4 +64,59 @@ async function dadosDiarios() {
 
 dadosDiarios();
 
-  
+
+// 4)
+
+const FaturamentoDistribuidora = [
+    {
+      "estado": "SP",
+      "valor": 67836.43
+    },
+    {
+      "estado": "RJ",
+      "valor": 36678.66
+    },
+    {
+      "estado": "MG",
+      "valor": 29229.88
+    },
+    {
+      "estado": "ES",
+      "valor": 27165.48
+    },
+    {
+      "estado": "Outros",
+      "valor": 19849.53
+    }
+]
+
+const fatDistribuidora = (dados) => {
+    const totalFaturamento = dados.reduce((soma, item) => soma + item.valor, 0);
+    // console.log(dados);
+
+    dados.forEach(({estado, valor})=>{
+        const porcentagem = (valor / totalFaturamento) * 100;
+        console.log(`Estado: ${estado}, Porcentagem: ${porcentagem.toFixed(2)}%`);
+    });
+
+    // console.log('tot',totalFaturamento);
+    
+}
+
+console.log(fatDistribuidora(FaturamentoDistribuidora));
+
+// 5)
+
+const reverseString = (str)=>{
+
+    let strInvetida = ''
+
+    for(let i = str.length - 1; i >= 0; i--) strInvetida += str[i]
+    
+    console.log(`String não invertida: ${str}`);
+    console.log(`String invertida: ${strInvetida}`);
+    
+}
+
+const strData = 'TRAGET'
+reverseString(strData)
